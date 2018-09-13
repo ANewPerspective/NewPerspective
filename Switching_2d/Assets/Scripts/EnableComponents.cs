3fcc8d6f7d3851c8597ccd346ba52b6e9df6d538
Cole Rosemergy <colerosemergy@gmail.com>
2018-09-13 15:13:08 -0400

    Adding all my files from the project

---
+++ Switching_2d/Assets/Scripts/EnableComponents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableComponents : MonoBehaviour {
+
+	public GameObject cube;
+	public GameObject sphere;
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.Space)) 
+		{
+			if (cube.activeInHierarchy == true) {
+				cube.SetActive (false);
+				sphere.SetActive (true);
+				sphere.transform.position = cube.transform.position;
+			} else {
+				cube.SetActive (true);
+				sphere.SetActive (false);
+				cube.transform.position = sphere.transform.position;
+			}
+		}
+	}
+}
